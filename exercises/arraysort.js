c72419dc33d8302b7089211607fc8379e9f6e3fa
blockerbella <patricemblocker@yahoo.com>
2019-05-16 09:08:38 -0600

    push Array Sort finally

--- exercises/arraysort.js
+++ exercises/arraysort.js
@@ -1,7 +1,54 @@
 //notes
+let myArry = leastToGreatest = [1, 3, 5, 2, 90, 20];
 
-function leastToGreatest(arr) {
-    let myArry = arr.sort();
-    return myArry;
+myArry.sort((a,b) => a-b);
+console.log(myArry);
+
+let myArry1 = leastToGreatest = [1, 3, 5, 2, 90, 20];
+
+myArry1.sort((a,b) => b-a);
+console.log(myArry1);
+
+let charArry = ["dog", "wolf", "by", "family", "eaten"];
+charArry.sort(function(a,b){
+    return a.length - b.length;
+});
+console.log(charArry);
+
+
+let alphaArry = ["dog", "wolf", "by", "family", "eaten"];
+
+function alpha(arr){
+    const sortArr = arr.sort((a,b) => {
+        return a > b;
+    });
+    return sortArr;
 }
-console.log(leastToGreatest([1, 3, 5, 2, 90, 20])); // [1, 2, 3, 5, 20, 90] 
+
+console.log(alpha(alphaArry));
+  
+const byAge = [
+      {
+          name: "Quiet Samurai",
+          age: 22
+      },
+      {
+          name: "Arrogant Ambassador",
+          age: 100
+      },
+      {
+          name: "Misunderstood Observer",
+          age: 2
+      },
+      {
+          name: "Unlucky Swami",
+          age: 77
+      }
+    ];
+
+
+byAge.sort(function(a,b){
+    return a.age - b.age;
+});
+
+console.log(byAge);
